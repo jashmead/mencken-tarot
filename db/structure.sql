c0667a7c4a1adaf5ea02f4879ff4be1f841bfe4e
jashmead <john.ashmead@ashmeadsoftware.com>
2013-08-15 10:55:07 -0400

    added foreign keys

--- db/structure.sql
+++ db/structure.sql
@@ -142,12 +142,154 @@ ALTER SEQUENCE people_id_seq OWNED BY people.id;
 
 
 --
+-- Name: plays; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE plays (
+    id integer NOT NULL,
+    play_order integer,
+    play_normal boolean,
+    play_tags character varying(255),
+    play_text character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone,
+    reading_id integer,
+    card_id integer
+);
+
+
+--
+-- Name: plays_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE plays_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: plays_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE plays_id_seq OWNED BY plays.id;
+
+
+--
+-- Name: positions; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE positions (
+    id integer NOT NULL,
+    position_order integer,
+    position_x integer,
+    position_y integer,
+    meaning_normal character varying(255),
+    meaning_reversed character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone,
+    spread_id integer
+);
+
+
+--
+-- Name: positions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE positions_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: positions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE positions_id_seq OWNED BY positions.id;
+
+
+--
+-- Name: readings; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE readings (
+    id integer NOT NULL,
+    name character varying(255),
+    description character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone,
+    person_id integer,
+    spread_id integer,
+    deck_id integer
+);
+
+
+--
+-- Name: readings_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE readings_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: readings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE readings_id_seq OWNED BY readings.id;
+
+
+--
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE TABLE schema_migrations (
     version character varying(255) NOT NULL
 );
+
+
+--
+-- Name: spreads; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE spreads (
+    id integer NOT NULL,
+    name character varying(255),
+    description character varying(255),
+    master_spread boolean,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone,
+    person_id integer,
+    deck_id integer
+);
+
+
+--
+-- Name: spreads_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE spreads_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: spreads_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE spreads_id_seq OWNED BY spreads.id;
 
 
 --
@@ -172,6 +314,34 @@ ALTER TABLE ONLY people ALTER COLUMN id SET DEFAULT nextval('people_id_seq'::reg
 
 
 --
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY plays ALTER COLUMN id SET DEFAULT nextval('plays_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY positions ALTER COLUMN id SET DEFAULT nextval('positions_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY readings ALTER COLUMN id SET DEFAULT nextval('readings_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY spreads ALTER COLUMN id SET DEFAULT nextval('spreads_id_seq'::regclass);
+
+
+--
 -- Name: cards_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -193,6 +363,38 @@ ALTER TABLE ONLY decks
 
 ALTER TABLE ONLY people
     ADD CONSTRAINT people_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: plays_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY plays
+    ADD CONSTRAINT plays_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: positions_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY positions
+    ADD CONSTRAINT positions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: readings_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY readings
+    ADD CONSTRAINT readings_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: spreads_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY spreads
+    ADD CONSTRAINT spreads_pkey PRIMARY KEY (id);
 
 
 --
@@ -240,3 +442,19 @@ INSERT INTO schema_migrations (version) VALUES ('20130814192749');
 INSERT INTO schema_migrations (version) VALUES ('20130814211633');
 
 INSERT INTO schema_migrations (version) VALUES ('20130814212749');
+
+INSERT INTO schema_migrations (version) VALUES ('20130814215859');
+
+INSERT INTO schema_migrations (version) VALUES ('20130814220336');
+
+INSERT INTO schema_migrations (version) VALUES ('20130814221615');
+
+INSERT INTO schema_migrations (version) VALUES ('20130814221735');
+
+INSERT INTO schema_migrations (version) VALUES ('20130814225543');
+
+INSERT INTO schema_migrations (version) VALUES ('20130814225811');
+
+INSERT INTO schema_migrations (version) VALUES ('20130814230225');
+
+INSERT INTO schema_migrations (version) VALUES ('20130814230617');
